e24cd87d084956215f6fdf5d824a3de382c4b16b
BlandineDupas <blandine.dupas@sfr.fr>
2021-03-26 17:50:44 +0100

    a pacman can move every direction

---
+++ app.js
@@ -0,0 +1,78 @@
+let app = {
+    // Properties
+    pacman: null,
+    direction: 'left',
+
+    // Methods
+    init: () => {
+        boardApp.init();
+        app.createPacman();
+        setTimeout(app.moveForward, 1000);
+        // setInterval(app.moveForward, 1000);
+    },
+
+    createPacman: () => {
+        app.pacman = document.querySelector('#row15 .col9');
+        app.pacman.setAttribute('id', 'pacman');
+        app.pacman.classList.add('pacman-left');
+    },
+
+    moveForward: () => {
+        let newPacman;
+
+        if (app.pacman.classList.contains('pacman-left')) {
+            newPacman = app.pacman.previousSibling;
+        }
+        
+        else if (app.pacman.classList.contains('pacman-right')) {
+            newPacman = app.pacman.nextSibling;
+        }
+        
+        else if (app.pacman.classList.contains('pacman-up')) {
+            let colNb = Object.values(app.pacman.classList).find(className => className.match(/^col[0-9]+$/));
+            let rowNb = app.pacman.closest('.row').previousSibling.id;
+            newPacman = document.querySelector('#' + rowNb + ' .' + colNb);
+        }
+        
+        else if (app.pacman.classList.contains('pacman-down')) {
+            let colNb = Object.values(app.pacman.classList).find(className => className.match(/^col[0-9]+$/));
+            let rowNb = app.pacman.closest('.row').nextSibling.id;
+            newPacman = document.querySelector('#' + rowNb + ' .' + colNb);
+        }
+
+        app.pacman.classList.remove('pacman-up', 'pacman-down', 'pacman-right','pacman-left');
+        app.pacman.removeAttribute('id');
+
+        newPacman.classList.add('pacman-' + app.direction);
+        newPacman.setAttribute('id', 'pacman');
+
+        app.pacman = newPacman;
+    },
+
+    turnUp: () => {
+        pacman.classList.remove('pacman-down', 'pacman-right','pacman-left')
+        pacman.classList.add('pacman-up');
+        app.direction = 'up';
+    },
+
+    turnDown: () => {
+        pacman.classList.remove('pacman-up', 'pacman-right','pacman-left')
+        pacman.classList.add('pacman-down');
+        app.direction = 'down';
+    },
+
+    turnRight: () => {
+        pacman.classList.remove('pacman-up', 'pacman-down','pacman-left')
+        pacman.classList.add('pacman-right');
+        app.direction = 'right';
+    },
+
+    turnLeft: () => {
+        pacman.classList.remove('pacman-up', 'pacman-down', 'pacman-right')
+        pacman.classList.add('pacman-left');
+        app.direction = 'left';
+    }
+
+}
+
+document.addEventListener('DOMContentLoaded', app.init);
--- boardApp.js
+++ boardApp.js
@@ -9,15 +9,15 @@ let boardApp = {
         boardApp.loadBoard();
     },
 
-    createWall: function () {
+    createWall: function (i) {
         let newWall = document.createElement('div');
-        newWall.classList.add('wall');
+        newWall.classList.add('wall', 'col' + i);
         return newWall;
     },
 
-    createRoad: function () {
+    createRoad: function (i) {
         let newRoad = document.createElement('div');
-        newRoad.classList.add('road');
+        newRoad.classList.add('road', 'col' + i);
         return newRoad;
     },
 
@@ -32,23 +32,25 @@ let boardApp = {
             // console.log(i, boardApp.map[i]);
             let rowMapStr = boardApp.map[rowIndex];
             let rowLength = boardApp.map[rowIndex].length;
+            
             let newRow = document.createElement('div');
             newRow.classList.add('row');
+            newRow.setAttribute('id', 'row' + rowIndex);
             boardApp.board.appendChild(newRow);
             
             for (let columnIndex = 0; columnIndex < rowLength; columnIndex++) {
                 // console.log(rowMapStr[columnIndex]);
                 if (rowMapStr[columnIndex] === 'w') {
                     // console.log('wall');
-                    let newWall = boardApp.createWall();
+                    let newWall = boardApp.createWall(columnIndex);
                     newRow.appendChild(newWall);
                 } else if (rowMapStr[columnIndex] === 'r') {
                     // console.log('road');
-                    let newRoad = boardApp.createRoad();
+                    let newRoad = boardApp.createRoad(columnIndex);
                     newRow.appendChild(newRoad);
                 } if (rowMapStr[columnIndex] === 'f') {
                     // console.log('food');
-                    let newRoad = boardApp.createRoad();
+                    let newRoad = boardApp.createRoad(columnIndex);
                     newRow.appendChild(newRoad);
                     let newFood = boardApp.createFood();
                     newRoad.appendChild(newFood);
